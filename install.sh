c1ab31c1d47d3b2f684e3506c64e9b6e8c6c33c9
MohammadSZ <mohammad.jalali021@gmail.com>
2017-02-17 18:53:57 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,27 @@
 #!/usr/bin/env bash
 wget "https://valtman.name/files/telegram-cli-1222"
-sudo apt-get install libreadline-dev -y
-sudo apt-get install libconfig-dev -y
-sudo apt-get install libssl-dev -y
-sudo apt-get install lua5.2 -y
-sudo apt-get install liblua5.2-dev -y
-sudo apt-get install lua-socket -y
-sudo apt-get install lua-sec -y
-sudo apt-get install lua-expat -y
-sudo apt-get install libevent-dev -y
-sudo apt-get install make -y
 sudo apt-get install g++-4.7 -y c++-4.7 -y
-sudo apt-get update		
+sudo apt-get update
 sudo apt-get upgrade
+sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
 sudo apt-get install lua-lgi -y
-sudo apt-get install libnotify-dev -y
+sudo apt-get install libstdc++6 -y
 sudo apt-get install screen -y
 sudo apt-get install tmux -y
-sudo apt-get install make -y
-sudo apt-get install unzip -y
-sudo apt-get install git -y
-sudo apt-get install redis-server -y
-sudo apt-get install autoconf -y
-sudo apt-get install g++ -y
-sudo apt-get install libjansson-dev -y
-sudo apt-get install libpython-dev -y
-sudo apt-get install expat libexpat1-dev -y
+sudo apt-get install libnotify-dev -y
 sudo apt-get install ppa-purge -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-dev -y
 sudo apt-get install software-properties-common -y
 sudo apt-get install python-software-properties -y
 sudo apt-get install gcc-6 -y
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo ppa-purge
-sudo pip3 install redis -y
-sudo service redis-server restart -y
+sudo pip3 install redis
+sudo service redis-server restart
 chmod 777 creator.py
 chmod 777 run.sh
 chmod 777 telegram-cli-1222
