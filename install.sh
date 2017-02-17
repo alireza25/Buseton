e6203875686723688da2127800278562709f135b
MohammadSZ <mohammad.jalali021@gmail.com>
2017-02-17 18:08:42 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,44 @@
 #!/usr/bin/env bash
 wget "https://valtman.name/files/telegram-cli-1222"
-sudo apt-get install libreadline-dev
-sudo apt-get install libconfig-dev 
-sudo apt-get install libssl-dev 
-sudo apt-get install lua5.2 
-sudo apt-get install liblua5.2-dev 
-sudo apt-get install lua-socket 
-sudo apt-get install lua-sec 
-sudo apt-get install lua-expat 
-sudo apt-get install libevent-dev 
-sudo apt-get install make 
-sudo apt-get install g++-4.7 c++-4.7
+sudo apt-get install libreadline-dev -y
+sudo apt-get install libconfig-dev -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install lua5.2 -y
+sudo apt-get install liblua5.2-dev -y
+sudo apt-get install lua-socket -y
+sudo apt-get install lua-sec -y
+sudo apt-get install lua-expat -y
+sudo apt-get install libevent-dev -y
+sudo apt-get install make -y
+sudo apt-get install g++-4.7 -y c++-4.7 -y
 sudo apt-get update		
 sudo apt-get upgrade
-sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev
-sudo apt-get install lua-lgi
-sudo apt-get install libnotify-dev
-sudo apt-get install screen
-sudo apt-get install tmux
-sudo apt-get install unzip 
-sudo apt-get install git 
-sudo apt-get install redis-server 
-sudo apt-get install autoconf 
-sudo apt-get install g++ 
-sudo apt-get install libjansson-dev 
-sudo apt-get install libpython-dev 
-sudo apt-get install expat libexpat1-dev 
-sudo apt-get install ppa-purge 
-sudo apt-get install python3-pip 
-sudo apt-get install python3-dev
-sudo apt-get install software-properties-common
-sudo apt-get install python-software-properties
-sudo apt-get install gcc-6
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install lua-lgi -y
+sudo apt-get install libnotify-dev -y
+sudo apt-get install screen -y
+sudo apt-get install tmux -y
+sudo apt-get install make -y
+sudo apt-get install unzip -y
+sudo apt-get install git -y
+sudo apt-get install redis-server -y
+sudo apt-get install autoconf -y
+sudo apt-get install g++ -y
+sudo apt-get install libjansson-dev -y
+sudo apt-get install libpython-dev -y
+sudo apt-get install expat libexpat1-dev -y
+sudo apt-get install ppa-purge -y
+sudo apt-get install python3-pip -y
+sudo apt-get install python3-dev -y
+sudo apt-get install software-properties-common -y
+sudo apt-get install python-software-properties -y
+sudo apt-get install gcc-6 -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo ppa-purge
-sudo pip3 install redis
-sudo service redis-server restart
+sudo pip3 install redis -y
+sudo service redis-server restart -y
 chmod 777 creator.py
 chmod 777 run.sh
 chmod 777 telegram-cli-1222
